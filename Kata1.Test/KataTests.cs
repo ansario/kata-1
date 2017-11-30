78257fc9425180eecec4221e552e4dbd60fd244b
Omar Ansari <ansario@email.sc.edu>
2017-11-29 22:53:56 -0500

    initial commit

---
+++ Kata1.Test/KataTests.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+
+namespace Kata1.Test
+{
+    [TestFixture]
+    class KataTests
+    {
+
+        Solution _solution;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            _solution = new Solution();
+        }
+
+        [Test]
+        public void NoDuplicatesReturnsCorrectResults()
+        {
+            var noDuplicates = new int[] { 1, 2, 5, 7, 9, 100, 2000, 1111, 2222, 0 };
+
+            var result = _solution.FindFirstDuplicate(noDuplicates);
+
+            Assert.AreEqual(result, -1);
+        }
+
+        [Test]
+        public void EmptyListReturnsCorrectResults()
+        {
+            var noValues = new int[] { };
+
+            var result = _solution.FindFirstDuplicate(noValues);
+
+            Assert.AreEqual(result, -1);
+        }
+
+        [Test]
+        public void NullListReturnsCorrectResults()
+        {
+            int[] nullList = null;
+
+            var result = _solution.FindFirstDuplicate(nullList);
+
+            Assert.AreEqual(result, -1);
+        }
+
+        [Test]
+        public void DuplicatesReturnsCorrectResults()
+        {
+            var threeDuplicates = new int[] { 1, 1, 2, 2, 3, 3 };
+            var twoDuplicates = new int[] { 1, 2, 1, 2 };
+
+            var resultThreeDuplicates = _solution.FindFirstDuplicate(threeDuplicates);
+            var resultTwoDuplicates = _solution.FindFirstDuplicate(twoDuplicates);
+
+            Assert.AreEqual(resultThreeDuplicates, 1);
+            Assert.AreEqual(resultTwoDuplicates, 1);
+        }
+    }
+}
---
+++ Kata1/Solution.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Kata1
+{
+    public class Solution
+    {
+        public int FindFirstDuplicate(int[] list)
+        {
+
+            if (list == null || !list.Any())
+            {
+                return -1;
+            }
+
+
+            var map = new Dictionary<int, bool>();
+
+            for(var i = 0; i < list.Length; i++)
+            {
+
+                bool seen;
+
+                if (map.TryGetValue(list[i], out seen))
+                {
+                    return list[i];
+                }
+
+                map[list[i]] = true;
+            }
+
+            return -1;
+        }
+    }
+}
